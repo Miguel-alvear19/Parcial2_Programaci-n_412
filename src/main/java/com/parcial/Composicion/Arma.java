0e52bd12ff8a7520e74d72944ee7532b415d6735
Miguel Londoño <magnolinneo1971@gmail.com>
2026-04-17 20:39:25 -0500

    Agregando los metodos de la clase Arma

--- src/main/java/com/parcial/Composicion/Arma.java
+++ src/main/java/com/parcial/Composicion/Arma.java
@@ -2,4 +2,36 @@ package com.parcial.Composicion;
 
 public class Arma {
 
+    private String nombre;
+    private int daño;
+
+    public Arma(String nombre, int daño) {
+        this.nombre = nombre;
+        this.daño = daño;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getDaño() {
+        return daño;
+    }
+
+    public void setDaño(int daño) {
+        this.daño = daño;
+    }
+
+    public void atacarConArma() {
+
+    }
+
+    public void getDañoAdicional() {
+
+    }
+
 }
