693e0f1e21f520d5e44419d4bdabd9a1c6b8746a
Miguel Londoño <magnolinneo1971@gmail.com>
2026-04-17 22:02:49 -0500

    se editan las clases concretas

--- src/main/java/com/parcial/ClaseConcreta/Dragon.java
+++ src/main/java/com/parcial/ClaseConcreta/Dragon.java
@@ -1,5 +1,32 @@
 package com.parcial.ClaseConcreta;
 
-public class Dragon {
+import com.parcial.Composicion.Arma;
+import com.parcial.Interfaces.IVolador;
+import com.parcial.claseAbstracta.Criatura;
 
+
+public class Dragon extends Criatura implements IVolador {
+    private String escamas;
+    private Arma arma;
+
+    public Dragon(String nombre, int salud, int fuerza, Arma arma) {
+        super(nombre, salud, fuerza);
+        this.escamas = "Rojas";
+        this.arma = arma;
+    }
+
+    @Override
+    public void atacar(Criatura objetivo) {
+        arma.atacarConArma(objetivo, fuerza);
+    }
+
+    @Override
+    public void volar() {
+        System.out.println(nombre + " está volando.");
+    }
+
+    @Override
+    public void aterrizar() {
+        System.out.println(nombre + " aterriza.");
+    }
 }
