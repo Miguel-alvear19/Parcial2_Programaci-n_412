743c06707fa10abbc063864735e40d3da1c34d60
Miguel Londoño <magnolinneo1971@gmail.com>
2026-04-17 20:31:59 -0500

    se crean los atributos y metodos de la clase Criatura

--- src/main/java/com/parcial/claseAbstracta/Criatura.java
+++ src/main/java/com/parcial/claseAbstracta/Criatura.java
@@ -2,4 +2,48 @@ package com.parcial.claseAbstracta;
 
 public abstract class Criatura {
 
+    protected String nombre;
+    protected int salud;
+    protected int fuerza;
+
+    public Criatura(String nombre, int salud, int fuerza) {
+        this.nombre = nombre;
+        this.salud = salud;
+        this.fuerza = fuerza;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getSalud() {
+        return salud;
+    }
+
+    public void setSalud(int salud) {
+        this.salud = salud;
+    }
+
+    public int getFuerza() {
+        return fuerza;
+    }
+
+    public void setFuerza(int fuerza) {
+        this.fuerza = fuerza;
+    }
+
+    public abstract void atacar(Criatura objetivo);
+
+    public abstract void defender(int daño);
+
+    public boolean estaViva() {
+        return salud > 0;
+    }
+
+    
+
 }
